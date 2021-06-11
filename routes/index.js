1d9636f9ba338aab01037502b17fc87f91ee8b53
rajulapudi <rajulapudip@gmail.com>
2021-06-12 02:04:35 +0530

    changes in content and styling

--- routes/index.js
+++ routes/index.js
@@ -1,54 +1,54 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var formidable = require('formidable');
-var fs = require('fs');
-var path = require('path');
+var formidable = require("formidable");
+var fs = require("fs");
+var path = require("path");
 
-const sendGridEmail = require('../service/Mail');
+const sendGridEmail = require("../service/Mail");
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-	res.render('index', { title: 'Learner Centric' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Learnercentric" });
 });
-router.get('/about', function (req, res, next) {
-	res.render('about', { title: 'Learner Centric' });
+router.get("/about", function (req, res, next) {
+  res.render("about", { title: "Learnercentric" });
 });
-router.get('/contact', function (req, res, next) {
-	res.render('contact', { title: 'Learner Centric' });
+router.get("/contact", function (req, res, next) {
+  res.render("contact", { title: "Learnercentric" });
 });
-router.get('/subjects', function (req, res, next) {
-	res.render('subjects', { title: 'Learner Centric' });
+router.get("/subjects", function (req, res, next) {
+  res.render("subjects", { title: "Learnercentric" });
 });
 
-router.post('/contct', function (req, res, next) {
-	try {
-		console.log('conatct route called');
+router.post("/contct", function (req, res, next) {
+  try {
+    console.log("conatct route called");
 
-		const form = new formidable.IncomingForm();
-		form.parse(req, async function (err, fields) {
-			const { name, email, subject, number, message } = fields;
-			console.log(name, email, subject, number, message);
+    const form = new formidable.IncomingForm();
+    form.parse(req, async function (err, fields) {
+      const { name, email, subject, number, message } = fields;
+      console.log(name, email, subject, number, message);
 
-			if (err) {
-				console.log(err);
-			} else {
-				let result = await sendGridEmail({
-					name: name,
-					email: email,
-					number: number,
-					subject: subject,
-					message: message,
-				});
-				if (result.status) {
-					res.status(200).send('OK');
-				} else {
-					res.status(500).send('Failed');
-				}
-			}
-		});
-	} catch (error) {
-		console.log(error);
-		res.status(500);
-	}
+      if (err) {
+        console.log(err);
+      } else {
+        let result = await sendGridEmail({
+          name: name,
+          email: email,
+          number: number,
+          subject: subject,
+          message: message,
+        });
+        if (result.status) {
+          res.status(200).send("OK");
+        } else {
+          res.status(500).send("Failed");
+        }
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+  }
 });
 module.exports = router;
